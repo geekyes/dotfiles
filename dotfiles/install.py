7550104975023b5487a157e3ef73d3517981bc2f
geekyes <chenjiangeek@163.com>
2018-03-31 15:48:20 +0800

    初次配置终端工具的配置文件，2018/3/31

---
+++ dotfiles/install.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import os
+
+# 安装zsh, vim, tmux
+def tool_install():
+    tool_install_list = ['zsh', 'vim-gtk3', 'tmux', 'curl', 'autojump']
+    if ('posix' == os.name):
+        for i in tool_install_list:
+            if (not os.path.exists('/usr/bin/' + i)):
+                if (os.system('sudo apt install ' + i)):
+                    print (i + ' install failure!!!\n')
+                    exit(-1)
+
+def oh_my_zsh_install():
+    home_dir = os.environ['HOME']
+    if (not os.path.exists(home_dir + '/.oh-my-zsh')):
+        print('install oh-my-zsh ...\n')
+        if (os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')):
+            print('oh-my-zsh install failure!!!\n')
+            exit(-1)
+
+def cfg():
+    script_dir = os.path.abspath('.')
+    home_dir = os.environ['HOME']
+    print('cfg ...')
+    print('''请手动添加
+        source /usr/share/autojump/autojump.sh
+        plugins = (git vi-mode autojump wd z)
+        到  ~/.zshrc 文件中''')
+    os.system('ln -s ' + script_dir + "/vimrc " + home_dir + '/.vimrc')
+    os.system('ln -s ' + script_dir + "/tmux.conf " + home_dir + '/.tmux.conf')
+
+
+if (__name__ == '__main__'):
+    tool_install()
+    cfg()
+    oh_my_zsh_install()
